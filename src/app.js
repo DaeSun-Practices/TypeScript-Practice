44c755d971a05e34cb3e5ee3970ea99353f04764
송대선 <songdaesun@songdaeseon-ui-MacBookPro.local>
2021-05-27 23:51:22 +0900

    done 3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
-var age = 10;
-console.log(age);
 function logName(name) {
     console.log(name);
 }
 logName("Hello World!");
+var studentID = 12345;
+var studentName = 'Junny Kim';
+var age = 21;
+var gender = true;
+var subject = "TypeScript";
+var courseCompleted = true;
+function getStudentDetails(studentID) {
+    return {
+        'studentID': studentID,
+        'studentName': studentName,
+        'age': age,
+        'gender': gender,
+        'subject': subject,
+        'courseCompleted': courseCompleted
+    };
+}
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
-let age:number  = 10;
-
-console.log(age);
-
 function logName (name : string){
     console.log(name);
 }
 
 logName("Hello World!");
+
+
+let studentID:number = 12345;
+let studentName:string = 'Junny Kim';
+let age:number = 21;
+let gender:boolean = true;
+let subject:string = "TypeScript";
+let courseCompleted:boolean = true;
+
+                                           
+function getStudentDetails(studentID: number)
+//return 되는 값의 type
+:{
+    studentID:number;
+    studentName:string;
+    age:number;
+    gender:boolean;
+    subject:string;
+    courseCompleted:boolean;
+}
+
+{
+    return {
+        'studentID':studentID,
+        'studentName':studentName,
+        'age':age,
+        'gender':gender,
+        'subject':subject,
+        'courseCompleted':courseCompleted
+    };
+}
