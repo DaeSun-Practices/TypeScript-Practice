8a9016ade17e8c3c901ed6961897b59cd1ff3b13
송대선 <songdaesun@songdaeseon-ui-MacBookPro.local>
2021-05-28 17:38:49 +0900

    done 7

--- src/app2.js
+++ src/app2.js
@@ -1,3 +1,17 @@
 var someValue = 5; //any type
 someValue = 'hello';
 someValue = true;
+var price = 5; // union type
+price = 'free';
+//                                       ?로 선택적 매개변수로 만들 수 있다.
+function sendGreeting(message, userName, familyName) {
+    if (familyName === void 0) { familyName = "There"; }
+    console.log(message + ", " + userName + ", " + familyName);
+}
+sendGreeting("Hello");
+// arrow function
+var sendGreeting2 = function (message, userName, familyName) {
+    if (familyName === void 0) { familyName = "There"; }
+    console.log(message + ", " + userName + ", " + familyName);
+};
+sendGreeting2("Hello");
--- src/app2.ts
+++ src/app2.ts
@@ -1,3 +1,24 @@
 let someValue: any = 5; //any type
 someValue = 'hello';
 someValue = true;
+
+let price: number | string = 5; // union type
+price = 'free'
+
+type StrOrNum = number | string; // tyoe aliases
+
+
+
+//                                       ?로 선택적 매개변수로 만들 수 있다.
+function sendGreeting (message : string, userName?: string, familyName = "There"):void{
+    console.log(`${message}, ${userName}, ${familyName}`);
+}
+
+sendGreeting("Hello")
+
+// arrow function
+const sendGreeting2 = (message : string, userName?: string, familyName = "There"):void => {
+    console.log(`${message}, ${userName}, ${familyName}`);
+}
+
+sendGreeting2("Hello")
